616c9e0174a901d809f99771f9329cf07b9b5c47
sutepjantawee <suthep@vertobase.com>
2022-03-09 10:20:38 +0700

    Login with username

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,67 +1,142 @@
-import * as React from 'react';
-import {Routes,Route, } from 'react-router-dom';
+import * as React from "react";
+import {Routes,Route,useNavigate,useLocation,Navigate,Outlet,} from "react-router-dom";
+import { fakeAuthProvider } from "./auth";
+import Axios from "axios"
+import { useEffect, useState } from "react";
+export default function App() {
+  return (
+    
+    <AuthProvider>
+      <Routes>
+        <Route element={<Layout />}> 
+          <Route path="/login" element={<Login/>} />
+          <Route
+            path="/protected"
+            element={
+              <RequireAuth>
+                <Username />
+              </RequireAuth>
+            }
+          />
+        </Route>
+      </Routes>
+    </AuthProvider>
+  );
+}
 
-import { useState, useMemo } from 'react';
-import { Link } from "react-router-dom";
-import { useContext } from 'react';
-import DataContext from './DataContext';
 
+let DataContext = React.createContext([]);
 
-function App() {
+function useAuth() {
+  return React.useContext(DataContext);
+}
 
-const [userName, setUserName] = useState([])
-const value = useMemo( ()=>({userName, setUserName}),[userName] );
+function Login() {
+  let navigate = useNavigate();
+  let location = useLocation();
+  let auth = useAuth();
+  let from = location.state?.from?.pathname || "/";
 
-   return (
- 
-     <div>
-       <DataContext.Provider value={value}>
-       <Routes>
-         <Route path="/" element={<Login />} />
-         <Route path="/user" element={<User />} />
-       </Routes>
-       </DataContext.Provider>
- 
-     </div>
- 
-   );
- }
- export default App;
- 
+  function handleSubmit(event) {
+    event.preventDefault();
 
-const Login = () => {
+    let formData = new FormData(event.currentTarget);
+    let username = formData.get("username") 
 
- const { userName, setUserName } = useContext(DataContext)
- const change = event => setUserName(event.target.value)
- 
+    auth.signin(username, () => {
+      navigate(from, { replace: true });
+    });
+  }
+
   return (
     <div>
-      
-      <form> Username: 
-        <input type="text" name="name" onChange={change} value={userName}/>
-        <br/>
-      <button>
-        <Link to="/user">Login</Link>     
-      </button>
+      <form onSubmit={handleSubmit}>
+          Username: <input name="username" type="text" />
+        <button type="submit">Login</button>
       </form>
-      
     </div>
-  )
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      <AuthStatus />
+      <Outlet />
+    </div>
+  );
 }
 
 
-const User = () => {
-  const {userName}=useContext(DataContext)
-  
+
+function AuthProvider({ children }) {
+  let [user, setUser] = React.useState(null);
+
+  let signin = (newUser, callback) => {
+    return fakeAuthProvider.signin(() => {
+      setUser(newUser);
+      callback();
+    });
+  };
+
+  let signout = (callback) => {
+    return fakeAuthProvider.signout(() => {
+      setUser(null);
+      callback();
+    });
+  };
+  let value = { user, signin, signout };
+
+  return <DataContext.Provider value={value}>{children}
+            </DataContext.Provider>;
+}
+
+
+function AuthStatus() {
+  let auth = useAuth();
+  let navigate = useNavigate();
+
+  if (!auth.user) {
+    return <div></div>;
+  }
+
   return (
-    <div> {userName} <br/>
-      <button>
-        <Link to="/">Logout</Link>
+    <div>
+      Welcome: {auth.user}{" "}
+      <button
+        onClick={() => {
+          auth.signout(() => navigate([]));
+        }}
+      >
+        Sign out
       </button>
     </div>
-  )
+  );
 }
 
+function RequireAuth({ children }) {
+  let auth = useAuth();
+  let location = useLocation();
 
+  if (!auth.user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  return children;
+}
 
+function Username() {
+useEffect(() => {
+  Axios.get('https://dog.ceo/api/breeds/image/random').then(response => {
+    setimgdog(response.data.message)
+    console.log(response.data.message)
+  })
+},[])
+  const [imgdog, setimgdog] = useState('');
 
+  
+  return <div>
+    <form>
+       <img src={imgdog}/>
+       </form>
+  </div>;
+}
